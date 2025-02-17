17d80521be7e007d504ac85a1abe912f10da95e4
QianFuv <qianfuv@qq.com>
2025-02-17 22:12:54 +1100

    ➖ 整合包：删除模组 Celestisynth、Diet、Village Artifacts、Witch Mobility

--- pack/kubejs/server_scripts/recipe.js
+++ pack/kubejs/server_scripts/recipe.js
@@ -1,27 +1,6 @@
 // 用于添加、修改物品配方
 
 ServerEvents.recipes(event => {
-    // 焰矢扫帚
-    event.replaceInput(
-        { output: "witchmobility:firebolt_broom_item" },
-        "minecraft:stick",
-        "minecraft:blaze_rod"
-    )
-    
-    // 银箭扫帚
-    event.replaceInput(
-        { output: "witchmobility:silver_arrow_broom_item" },
-        "minecraft:hay_block",
-        "minecraft:nether_star"
-    )
-
-    // 粗制绿宝石碎片
-    event.replaceInput(
-        { output: "villagertools:restock" },
-        "minecraft:emerald",
-        "minecraft:emerald_block"
-    )
-
     // 镀层升级
     event.replaceInput(
         { output: "modulargolems:coating" },
